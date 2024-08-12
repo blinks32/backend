64b0d060a40d66ee85ae5f4d93ae66ee7e78e943
Chinedu Etoh <chndth@gmail.com>
2024-08-12 20:21:18 +0300

    n

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000; // Ensure the port is defined
+
+const corsOptions = {
+  origin: 'https://my-website-91lv.vercel.app',
+  methods: 'GET,POST',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(bodyParser.json());
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+function authenticateToken(req, res, next) {
+  const token = req.headers['authorization'];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
 app.post('/generate', async (req, res) => {
   const { context, prompt } = req.body;
   try {
@@ -35,4 +69,9 @@ app.post('/generate-image', authenticateToken, async (req, res) => {
     console.error('Error generating image:', error);
     res.status(500).send(error.message);
   }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
